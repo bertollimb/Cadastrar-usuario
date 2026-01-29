2a494e51f9360de063a9dbb64be4c31b11631e77
Matheus bertolli <matheusbertollianastacio@gmail.com>
2026-01-29 22:35:59 +0000

    update

--- programa-de-cadastro/cadastrar-usuario.py
+++ programa-de-cadastro/cadastrar-usuario.py
@@ -1,31 +1,23 @@
-usuarios = []
-
-def cadastrar_usuario():
-    cadastro_usuario = {}
-    cadastro_usuario['nome'] = input('Nome: ').title()
-    cadastro_usuario['idade'] = int(input('Idade: '))
-    cadastro_usuario['email'] = input('Email: ')
-    return cadastro_usuario
-
-def mostrar_usuarios():
-    if not usuarios:
-        print('Nenhum usuário cadastrado!')
-    else:
-        print(f"{' USUÁRIOS CADASTRADOS ':-^40}")
-        for i, p in enumerate(usuarios, 1):
-            print(f"{i}. Nome: {p['nome']} | Idade: {p['idade']} | Email: {p['email']}")
-
-while True:
-    cadastro = input('Cadastrar novo usuário? (S/N): ').upper()
-    if cadastro == 'N':
-        mostrar_usuarios()
-        break
-    elif cadastro == 'S':
-        usuarios.append(cadastrar_usuario())
-    else:
-        print('Opção inválida! Digite S ou N.')
 
 
 
 
-    
+from flask import Flask, render_template, request, redirect, url_for
+
+app = Flask(__name__)
+
+cadastros = []
+@app.route('/', methods=['GET', 'POST'])
+def principal():
+    if request.method == 'POST':
+        if request.form.get("nome") and request.form.get("idade") and request.form.get("email"):
+            cadastros.append({
+                "nome": request.form.get("nome"),
+                "idade": int(request.form.get("idade")), 
+                "email": request.form.get("email")
+                })
+        return redirect(url_for('principal'))
+    return render_template('index.html', cadastros=cadastros)
+
+if __name__ == '__main__':
+    app.run(debug=True)
